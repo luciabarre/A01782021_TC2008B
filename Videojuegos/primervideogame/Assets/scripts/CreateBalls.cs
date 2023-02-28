a7dfa57f19ec30be6f17b63239556edd76df297d
Lucia Barrenechea <lbarrenecheac@gmail.com>
2023-02-28 12:39:07 -0600

    generating balls in random places

--- Videojuegos/primervideogame/Assets/scripts/CreateBalls.cs
+++ Videojuegos/primervideogame/Assets/scripts/CreateBalls.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class CreateBalls : MonoBehaviour
 {
     //Class variables to be changed from the unity interface
-    [SerailizeField] GameObject ball;
+    [SerializeField] GameObject ball;
     // Start is called before the first frame update
     void Start()
     {
         //It calls the dropball fucntion after 0.5 seconds
         //and then it keeps calling it every 0.8 seconds
-        InvokeRepeating("DropBall()",0.5f, 0.8f)
+        InvokeRepeating("DropBall",0.5f, 0.8f);
     }
 
     // Update is called once per frame
@@ -20,8 +20,10 @@ public class CreateBalls : MonoBehaviour
     }
 
     void DropBall(){
+        //Generate a new random position
+        Vector3 pos = new Vector3(Random.Range(-10.0f, 10.0f)6,0)
         //Create a copy of the prefab
-        Instantiate(ball);
+        Instantiate(ball, pos, Quaternion.identity);
 
     }    
 }
