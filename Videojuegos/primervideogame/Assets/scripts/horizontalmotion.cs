a8a966a3496582240c6c74d20f05b3ea04066785
Lucia Barrenechea <lbarrenecheac@gmail.com>
2023-03-01 11:41:47 -0600

    new change sin file

--- Videojuegos/primervideogame/Assets/scripts/horizontalmotion.cs
+++ Videojuegos/primervideogame/Assets/scripts/horizontalmotion.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class horizontalmotion : MonoBehaviour
 {
+    Vector3 move;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,8 @@ public class horizontalmotion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        move.x=Input.GetAxis("Horizontal");
+        Debug.Log("X motion: "+ move.x);
+        transform.Translate(move);
     }
 }
