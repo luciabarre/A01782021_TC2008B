18815af746e60fb822e4df2953926fdf3f7d68ae
Lucia Barrenechea <lbarrenecheac@gmail.com>
2023-03-01 11:56:54 -0600

    finisihed script

--- Videojuegos/primervideogame/Assets/scripts/horizontalmotion.cs
+++ Videojuegos/primervideogame/Assets/scripts/horizontalmotion.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 
 public class horizontalmotion : MonoBehaviour
 {
+    [SerializeField] float speed;
+    [SerializeField] float limit;
     Vector3 move;
     // Start is called before the first frame update
     void Start()
@@ -17,7 +19,15 @@ public class horizontalmotion : MonoBehaviour
     void Update()
     {
         move.x=Input.GetAxis("Horizontal");
-        Debug.Log("X motion: "+ move.x);
-        transform.Translate(move);
+        //Debug.Log("X motion: "+ move.x);
+
+        if(transform.position.x < -limit && move.x < 0){
+            move.x=0;
+        }
+        else if(transform.position.x > limit && move.x > 0){
+            move.x=0;
+        }
+        //Esta función permite deterimanr la velocidad para que lado en el tiempo transcurrido.
+        transform.Translate(move * speed * Time.deltaTime);
     }
 }
