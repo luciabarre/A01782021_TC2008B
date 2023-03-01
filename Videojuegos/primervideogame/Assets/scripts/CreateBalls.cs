937542643bb43806d47af42da070cc972d8a3555
Lucia Barrenechea <lbarrenecheac@gmail.com>
2023-02-28 19:23:40 -0600

    new changes

--- Videojuegos/primervideogame/Assets/scripts/CreateBalls.cs
+++ Videojuegos/primervideogame/Assets/scripts/CreateBalls.cs
@@ -6,12 +6,13 @@ public class CreateBalls : MonoBehaviour
 {
     //Class variables to be changed from the unity interface
     [SerializeField] GameObject ball;
+    [SerializeField] float delay;
     // Start is called before the first frame update
     void Start()
     {
         //It calls the dropball fucntion after 0.5 seconds
         //and then it keeps calling it every 0.8 seconds
-        InvokeRepeating("DropBall",0.5f, 0.8f);
+        InvokeRepeating("DropBall",0.5f, delay);
     }
 
     // Update is called once per frame
@@ -21,9 +22,11 @@ public class CreateBalls : MonoBehaviour
 
     void DropBall(){
         //Generate a new random position
-        Vector3 pos = new Vector3(Random.Range(-10.0f, 10.0f)6,0);
+        Vector3 pos = new Vector3(Random.Range(-10.0f, 10.0f),6,0);
         //Create a copy of the prefab
-        Instantiate(ball, pos, Quaternion.identity);
+        GameObject obj=Instantiate(ball, pos, Quaternion.identity);
+        //doom the object to die in 5 seconds
+        Destroy(obj,5);
 
     }    
 }
