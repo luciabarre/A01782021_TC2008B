50f632b5e5c81895f05ae101ee5f0b19d9b4bc8b
Lucia Barrenechea <lbarrenecheac@gmail.com>
2023-03-01 12:37:40 -0600

    created Ballin and score c# files

---
+++ Videojuegos/primervideogame/Assets/scripts/score.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class score : MonoBehaviour
+{
+    int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score=0;
+        
+    }
+
+    // Update is called once per frame
+    public void AddPoints(int amount){
+        score+=amount;
+        Debug.Log("New score: "+ score);
+    }
+
+}
