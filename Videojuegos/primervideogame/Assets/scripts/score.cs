31443f3952b6f83b3f9cf12982f1316f77a5b76a
Lucia Barrenechea <lbarrenecheac@gmail.com>
2023-03-07 12:01:41 -0600

    new comits

--- Videojuegos/primervideogame/Assets/scripts/score.cs
+++ Videojuegos/primervideogame/Assets/scripts/score.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class score : MonoBehaviour
 {
+    [SerializeField] TextMeshPro tmpobj;
     int Score;
     // Start is called before the first frame update
     void Start()
